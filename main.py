a10a732b7e0968ab2fd7f49bb05eba8d31d0fade
Aerith Netzer <aerithnetzer@outlook.com>
2025-10-08 11:33:18 -0500

    Update main file to implement a reset feature

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fireworks.core.firework import Firework, Workflow
 from fireworks.user_objects.firetasks.script_task import PyTask
 import glob
 import argparse
+from tqdm import tqdm
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -16,13 +18,13 @@ parser.add_argument(
     "--recursive",
     "-r",
     help="If this argument is passed, adds files recursively to the workflow.",
+    action="store_true",
 )
 
 parser.add_argument(
     "--identifier",
     "-id",
     help="The identifier for which the files should be linked. This should be a barcode.",
-    action="store_true",
 )
 
 parser.add_argument(
@@ -32,14 +34,22 @@ parser.add_argument(
     required=False,
 )
 
+parser.add_argument(
+    "--reset",
+    help="Forces reset of all data. If you do not have admin access, this will fail.",
+    required=False,
+    action="store_true",
+)
+
 args = parser.parse_args()
 
 input_path = args.input
 is_recursive = args.recursive
 identifier = args.identifier
+fw_name = args.fw_name
+do_reset = args.reset
 
 
-conn_str: str
 conn_str = str(os.getenv("MONGODB_OCR_DEVELOPMENT_CONN_STRING"))
 lp = LaunchPad(
     host=conn_str,
@@ -55,6 +65,9 @@ fp = FilePad(
     database="fireworks",
 )
 
+if do_reset:
+    lp.reset("2025-10-08", require_password=False)
+    fp.reset()
 
 suffixes = ["jpg", "png", "jpeg", "tiff", "jp2"]
 
@@ -74,37 +87,35 @@ else:
             glob.glob(os.path.join(input_path, "**", f"*.{suffix}"), recursive=True)
         )
     identifiers = []
-    for f in sorted(files):
-        print(f)
+    for f in tqdm(sorted(files)[:3], desc="Uploading files..."):
         name = f.split("/")[-1]
-        file_id, identifier = fp.add_file(
+        new_identifier = str(identifier) + name
+        file_id, _ = fp.add_file(
             f,
-            identifier=str(identifier + name),
+            identifier=new_identifier,
             metadata={"source_path": f, "identifier": identifier, "filename": name},
         )
-        print(f"{identifier}")
-        identifiers.append(identifier)
-
+        identifiers.append(new_identifier)
     fw = Firework(
         [
             PyTask(
                 func="auxiliary.image_conversion_task",
                 inputs=[
                     "identifiers",
-                    "barcode_dir",
+                    "identifier",
                 ],
                 outputs="converted_images",
             ),
             PyTask(
                 func="auxiliary.image_to_pdf",
-                inputs=["converted_images", "barcode_dir"],
+                inputs=["converted_images", "identifier"],
                 outputs="PDF_id",
             ),
-            PyTask(func="auxiliary.marker_on_pdf", inputs=["PDF_id"]),
+            PyTask(func="auxiliary.marker_on_pdf", inputs=["PDF_id", "identifier"]),
         ],
-        name=identifier,
-        spec={"identifiers": identifiers, "barcode_dir": barcode_dir},
+        name=fw_name,
+        spec={"identifiers": identifiers, "identifier": identifier},
     )
-    wf = Workflow([fw], metadata={"barcode": barcode}, name=(barcode + "_workflow"))
+    wf = Workflow([fw], metadata={"identifier": identifier}, name=identifier)
 
     lp.add_wf(wf)
