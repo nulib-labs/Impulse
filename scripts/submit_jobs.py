ba0696da63a5ecb50d1ddd1389752ba412507e16
aerithnetzer <aerithnetzer@outlook.com>
2026-04-10 15:51:44 -0500

    Sample jobs

--- scripts/submit_jobs.py
+++ scripts/submit_jobs.py
@@ -1,14 +1,25 @@
-import os
-from fireworks import Firework, FWorker, LaunchPad, ScriptTask, TemplateWriterTask, FileTransferTask
+from fireworks import Firework, LaunchPad, Workflow
 from fireworks.core.rocket_launcher import launch_rocket
-import boto3
-from tasks.my_tasks import DocumentExtractionTask, ImageProcessingTask
 from tasks.mets import METSXMLToHathiTrustManifestTask
+from tasks.my_tasks import DocumentExtractionTask
+from tasks.config import MONGO_URI
+import boto3
 
-# # set up the LaunchPad and reset it
-launchpad: LaunchPad = LaunchPad(uri_mode=True, host=os.getenv("MONGODB_OCR_DEVELOPMENT_CONN_STRING_IMPULSE"), name="fireworks")
+launchpad: LaunchPad = LaunchPad(uri_mode=True, host=MONGO_URI, name="fireworks")
 
-# # create the Firework consisting of multiple tasks
-fw = Firework(METSXMLToHathiTrustManifestTask(), spec={'impulse_identifier':"P0491_35556036056489", "s3_xml_path": "s3://nu-impulse-production/P0491_35556036056489/mets.xml", "s3_yaml_path": "s3://nu-impulse-production/P0491_35556036056489/mets.yaml"})
-fw_id = fw.fw_id
-launchpad.add_wf(fw)
+
+client = boto3.client('s3', region_name='us-west-2')
+paginator = client.get_paginator('list_objects_v2')
+operation_parameters = {'Bucket': 'nu-impulse-production',
+                                        'Prefix': 'P0491_35556036056489/SOURCE/jpg/'}
+
+
+page_iterator = paginator.paginate(**operation_parameters)
+impulse_keys: list[str] = []
+for page in page_iterator:
+    for i in page['Contents']:
+        print(i['Key'])
+        impulse_keys.append(f"s3://nu-impulse-production/{i['Key']}")
+
+ocr_fw: Firework = Firework(DocumentExtractionTask(), spec={'impulse_identifier': "P0491_35556036056489", "find_path_array_in": "keys", "keys": sorted(impulse_keys)}, name="Document Extraction Workflow")
+launchpad.add_wf(ocr_fw)
